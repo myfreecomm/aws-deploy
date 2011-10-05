d6d974321393fc22745d9b2ecb20c9c381608d6d
Marcos Tapajos <tapajos@gmail.com>
2011-10-05 14:05:13 -0300

    Verifica se alguém está com problema

--- lib/aws-deploy/load_balance.rb
+++ lib/aws-deploy/load_balance.rb
@@ -1,7 +1,13 @@
 module AwsDeploy
   class LoadBalance
-    def find_by_name(credentials, name)
-      Aws::Elb.new(credentials.key, credentials.token).describe_load_balancers.detect {|lb| lb[:load_balancer_name] == name}
+    def initialize(credentials, name)
+      @credentials = credentials
+      @name = name
+    end
+    def instances
+      @ec2 = Aws::Ec2.new(@credentials.key, @credentials.token)
+      instance_ids = Aws::Elb.new(@credentials.key, @credentials.token).describe_load_balancers({:names => [@name]}).first[:instances].map{|i| i[:instance_id]}
+      @ec2.describe_instances(instance_ids)
     end
   end
 end
