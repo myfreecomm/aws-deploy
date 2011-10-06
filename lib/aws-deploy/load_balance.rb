c1a8c3f90d065bdf35696dbd5ba025b8723bc0df
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2011-10-06 16:42:35 -0300

    Evita trazer todas as instâncias por acidente.

--- lib/aws-deploy/load_balance.rb
+++ lib/aws-deploy/load_balance.rb
@@ -1,12 +1,13 @@
 module AwsDeploy
   class LoadBalance
-    def initialize(credentials, name)
+    def initialize(credentials, environment)
       @credentials = credentials
-      @name = name
+      @environment = environment
     end
     def instances
+      raise "no environment given" if @environment.nil? || @environment == ""
       @ec2 = Aws::Ec2.new(@credentials.key, @credentials.token)
-      instance_ids = Aws::Elb.new(@credentials.key, @credentials.token).describe_load_balancers({:names => [@name]}).first[:instances].map{|i| i[:instance_id]}
+      instance_ids = Aws::Elb.new(@credentials.key, @credentials.token).describe_load_balancers({:names => [@environment]}).first[:instances].map{|i| i[:instance_id]}
       @ec2.describe_instances(instance_ids)
     end
   end
