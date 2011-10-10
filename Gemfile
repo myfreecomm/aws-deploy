cc519de344434ccc951e19984e5266b99b05e96b
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2011-10-10 18:53:54 -0300

    Conserta limpeza de cache.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'http://rubygems.org'
 
+gem 'rake', '0.8.7'
+
 # Specify your gem's dependencies in aws-deploy.gemspec
 gemspec
--- lib/aws-deploy/cache.rb
+++ lib/aws-deploy/cache.rb
@@ -1,18 +1,21 @@
 module AwsDeploy
   class Cache
+    
     def initialize(credentials, environment)
       @credentials = credentials
       @environment = environment
     end
+    
     def clear
       instance = Instance.new(@credentials, @environment).find_all_in_service.first
       `ssh #{instance[:dns_name]} 'cd /srv/myfinance/src ; sudo RAILS_ENV=#{project_environment} /var/lib/gems/1.9.2/bin/bundle exec rake cache:clear'`
     end
+    
     private
-    def project_environment
-      environment = @environment.split("-").last.downcase
-      return 'staging' if environment == 'sandbox'
-      environment
-    end
+    
+      def project_environment
+        @environment.split("-").last.downcase
+      end
+    
   end
 end
